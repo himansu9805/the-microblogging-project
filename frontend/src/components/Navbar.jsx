3a8da3ebaa0b816ae1c3b32e35628f2932bd7320
Himansu <35388216+himansu9805@users.noreply.github.com>
2023-05-09 13:00:38 +0530

    Changed design

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,25 +1,54 @@
+import React from "react";
 import Logo from "../assets/images/logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 
-export const Navbar = () => {
+export default function Navbar() {
+  const [navbarOpen, setNavbarOpen] = React.useState(false);
+
   return (
-    <nav className="py-8">
-      <div className="flex flex-row items-center">
-        <img src={Logo} alt="Logo" className="h-10 pr-5" />
-        <div className="text-sm">
-          The
-          <br />
-          Microblogging Project
+    <>
+      <nav className="relative flex flex-wrap items-center justify-between px-2 py-8">
+        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+          <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+            <div className="flex flex-row items-center">
+              <img src={Logo} alt="Logo" className="h-10 pr-5" />
+              <div className="hidden md:block text-sm">
+                The
+                <br />
+                Microblogging Project
+              </div>
+            </div>
+            <button
+              className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+              type="button"
+              onClick={() => setNavbarOpen(!navbarOpen)}
+            >
+              {navbarOpen ? (
+                <FontAwesomeIcon icon={faXmark} />
+              ) : (
+                <FontAwesomeIcon icon={faBars} />
+              )}
+            </button>
+          </div>
+          <div
+            className={
+              "lg:flex flex-grow justify-center md:justify-end items-center" +
+              (navbarOpen ? " flex" : " hidden")
+            }
+            id="example-navbar-danger"
+          >
+            <div className="flex md:flex-row mt-5 flex-col justify-end items-center">
+              <Navitem value={"Home"} />
+              <Navitem value={"About"} />
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="flex md:flex-row flex-col justify-end items-center">
-        <Navitem value={"Home"} />
-        <Navitem value={"About"} />
-        <Navitem value={"Login"} />
-      </div>
-    </nav>
+      </nav>
+    </>
   );
-};
+}
 
 const Navitem = (props) => {
-  return <div className="md:pl-10">{props.value}</div>;
+  return <div className="py-1 md:py-0 md:pl-10 cursor-pointer">{props.value}</div>;
 };
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,39 +1,40 @@
 import React from "react";
 import StarsBackground from "../components/StarsBackground";
 
-import "../assets/styles/home.scss"
+import "../assets/styles/home.scss";
 
 export const Home = () => {
-
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 mt-10">
-      <StarsBackground />
-      <div className="flex flex-col my-0 md:my-4 items-center">
-        <div className="py-6 w-full md:w-3/4 text-center md:text-start">
-          <h1 className="text-3xl md:text-5xl py-2">To the moon?</h1>
-          <h1 className="text-3xl md:text-5xl py-2">Why limit yourself?</h1>
-          <h1 className="text-3xl md:text-5xl py-2">Join the stars!</h1>
-        </div>
-        <div className="py-6 text-center md:text-start">
-          <p className="text-xl md:text-2xl">Don't let anyone hold you back</p>
-          <p className="text-xl md:text-2xl">
-            Join our community today and reach for the stars!
-          </p>
-        </div>
+    <div className="flex flex-col my-0 md:my-4 items-center">
+        <StarsBackground />
+        <div className="flex flex-col my-0 md:my-4 items-center">
+          <div className="py-6 w-full text-center">
+            <h1 className="text-3xl md:text-5xl py-2">To the moon?</h1>
+            <h1 className="text-3xl md:text-5xl py-2">Why limit yourself?</h1>
+            <h1 className="text-3xl md:text-5xl py-2">Join the stars!</h1>
+          </div>
+          <div className="py-6 text-center">
+            <p className="text-xl md:text-2xl">
+              Don't let anyone hold you back
+            </p>
+            <p className="text-xl md:text-2xl">
+              Join our community today and reach for the stars!
+            </p>
+          </div>
       </div>
-      <div className="flex flex-col my-4 justify-around items-center">
-        <div className="w-full md:w-3/4">
-          <button className="btn-home font-bold text-lg py-5 md:py-10 rounded-3xl w-full uppercase">
-            Sign Up
-          </button>
+      <div className="py-6 w-full flex flex-col md:flex-row my-4 justify-center items-center">
+          <div className="w-full md:w-1/4">
+            <button className="btn-home font-bold text-lg py-5 md:py-10 rounded-3xl w-full uppercase">
+              Sign Up
+            </button>
+          </div>
+          <div className="h-10 md:w-64"></div>
+          <div className="w-full md:w-1/4">
+            <button className="btn-home font-bold text-lg py-5 md:py-10 rounded-3xl w-full uppercase">
+              Login
+            </button>
+          </div>
         </div>
-        <div className="h-10"></div>
-        <div className="w-full md:w-3/4">
-          <button className="btn-home font-bold text-lg py-5 md:py-10 rounded-3xl w-full uppercase">
-            Login
-          </button>
-        </div>
-      </div>
     </div>
   );
 };
